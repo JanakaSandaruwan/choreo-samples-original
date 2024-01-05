7eac6154b784658dcb4718f9cff985c8915c873a
Janaka Sandaruwan <janakasandaruwan1996@gmail.com>
2024-01-05 12:25:04 +0530

    update app

--- HelloWebApp/Program.cs
+++ HelloWebApp/Program.cs
@@ -1,5 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Host.UseUrls("http://0.0.0.0:5000", "https://0.0.0.0:5001");
+
 // Add services to the container.
 builder.Services.AddRazorPages();
 
